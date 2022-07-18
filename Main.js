05f8654a602fbfb54e9564e3fa046902d1a020cb
HelloWorld-n <gajica.b@gmail.com>
2022-07-18 15:59:22 +0200

    modified:   Main.js

--- Main.js
+++ Main.js
@@ -12,13 +12,17 @@ let x = "";
 	}, rndSample)
 })(
 	...(() => {
-		str = ""
-		cmds = []
+		let str = ""
+		let cmds = []
 		for (let chr of "---") {
 			str += chr
-			cmds[cmds.length] = (() => {
-				x += str
-			})
+			console.log(str)
+			cmds[cmds.length] = (eval(`() => {
+				x += "${str}"
+			}`))
+			for (let cmd of cmds) {
+				cmd()
+			}
 		}
 		return cmds
 	})(),
